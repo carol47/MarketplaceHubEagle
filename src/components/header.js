33caf9ca11d17c01eff5e1b30a92194404d6579f
Ana Carolina Salles <carol47@mbp-de-mariana.localdomain>
2020-06-14 18:48:13 +0200

    Implementacao de prototipo de layout

--- src/components/header.js
+++ src/components/header.js
@@ -5,29 +5,112 @@ import { css } from '@emotion/core';
 
 const Header = ({ siteTitle }) => (
   <header>
-    <div css={css`
+    <Nav>
+      <Title name={siteTitle} />
+      <Navigation />
+    </Nav>
+  </header>
+)
+
+
+const Nav = props => {
+
+  return (
+    <nav css={css`
+      height: 10vh;
+      width: 100vw;
       display: flex;
       flex-direction: row;
       align-items: center;
       color: #fff;
       width: 100%;
-      height: 96px;
-      background-image: linear-gradient(90deg, rgb(192, 45, 40), rgb(230, 98, 37));
+      height: 64px;
+      background-color: #7ccdca;
       position: relative;
-      border-radius: 5px;
       margin: 0px;
 
     `}>
-      <h1 css={css`
-        margin: 10px 20px 20px 20px;
+      {props.children}
+    </nav>
+  );
 
-      `}>
-          {siteTitle}
-      </h1>
+}
+
+const Title = props => {
+
+  return (
+    <h1 css={css`
+        width: 20vw;
+        margin-left: 16px;
+        color: black;
+        font-family: serif;
+        font-style: italic;
+        padding: 10px;
+
+  `}>{props.name}</h1>
+  );
+
+}
+
+const Navigation = props => {
+
+  return (
+    <div css={css`
+      width: 80vw;
+      margin: 0px 30px;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      color: black;
+
+    `}>
+      <div>Caminho / Caminho</div>
+      <UserProfile name="Fulanx - Usiminas"/>
+      <MenuBar />
     </div>
-  </header>
-)
-// linear-gradient(90deg, rgb(192, 45, 40), rgb(230, 98, 37))
+  );
+
+}
+
+const UserProfile = props => {
+  return (
+    <div css={css`
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      * {
+        padding: 0px 6px;
+      }
+    `}>
+      <div>
+        <img src="http://placeimg.com/64/64/people" alt="User Image"  css={css`
+          width: 48px;
+          border-radius: 50%;
+        `}/>
+      </div>
+      <div>{props.name}</div>
+    </div>
+
+  );
+}
+
+const MenuBar = props => {
+
+  return (
+    <div css={css`
+      display: flex;
+      flex-direction: row;
+      * {
+        padding: 6px;
+      }
+    `}>
+      <div>Minha Conta</div>
+      <div>Logout</div>
+    </div>
+  );
+
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
